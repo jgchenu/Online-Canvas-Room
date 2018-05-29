9f5d76be877ab442d2fa7f6cc5cfaf4db26d0596
jgchenu@foxmail.com <jgchenu@foxmail.com>
2018-05-29 18:45:54 +0800

    更新了

---
+++ src/pages/enter/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './index'
+
+const app = new Vue(App)
+app.$mount()
+
+export default {
+  config: {
+    navigationBarTitleText: '画板房间',
+    navigationBarBackgroundColor: '#6A7D8E',
+  }
+}
---
+++ src/pages/loginButton/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="loginPage">
+            <button @getuserinfo="onGetUserinfo" open-type="getUserInfo" class="loginButton">用户授权</button>
+            
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onGetUserinfo(e) {
+      //   console.log(e.detail.errMsg);
+      //   console.log(e.detail.userInfo);
+      //   console.log(e.detail.rawData);
+      console.log(e);
+      wx.openSetting({success:(res)=>{console.log(res);}});
+      // wx.navigateTo({
+      //   url: "../enter/main"
+      // });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.loginPage{
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .loginButton{
+        margin-top: 400rpx;
+        width: 200rpx;
+        height: 100rpx;
+    }
+}
+</style>
+
