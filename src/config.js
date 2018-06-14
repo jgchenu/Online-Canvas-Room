3b7582dca06da70910fa8425d301c4d3bd06bc24
jgchenu <jgchenu@foxmail.com>
2018-06-15 01:31:59 +0800

    恢复config

--- src/config.js
+++ src/config.js
@@ -2,27 +2,27 @@
  * 小程序配置文件, 使用前更名为config.js
  */
 
-var host = '';
+var host = 'https://board.upwki.com/api/weapp';
 
 
 var config = {
 
-    // 下面的地址配合云端 Demo 工作
-    service: {
-        host,
+  // 下面的地址配合云端 Demo 工作
+  service: {
+    host,
 
-        // 登录地址，用于建立会话
-        loginUrl: `${host}/login`,
+    // 登录地址，用于建立会话
+    loginUrl: `${host}/login`,
 
-        // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/user`,
+    // 测试的请求地址，用于测试会话
+    requestUrl: `${host}/user`,
 
-        // 测试的信道服务地址
-        tunnelUrl: `${host}/tunnel`,
+    // 测试的信道服务地址
+    tunnelUrl: `${host}/tunnel`,
 
-        // 上传图片接口
-        uploadUrl: `${host}/upload`
-    }
+    // 上传图片接口
+    uploadUrl: `${host}/upload`
+  }
 };
 
 module.exports = config;
